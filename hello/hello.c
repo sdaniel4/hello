24758f9e8023e0974f0092ae28f729ace4933167
sdaniel4 <46718538+sdaniel4@users.noreply.github.com>
2019-01-17 10:12:24 -0600

    Add files via upload

---
+++ hello/hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Sandeep, Salinas
+ * Date: 2019/15/01
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Sandeep, Salinas!\n");
+
+  return 0;
+}
